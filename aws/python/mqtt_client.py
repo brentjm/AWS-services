58d3beed2ec8090ba84917ae896415311aea4bc4
brentjm <brent_maranzano@yahoo.com>
2020-06-26 07:12:29 -0400

    Working version. Version works, but needs work on the socket. Possibly write the MQTT and socket as different classes.

--- aws/python/mqtt_client.py
+++ aws/python/mqtt_client.py
@@ -13,31 +13,33 @@ class MQTTClient(object):
     """
 
     def __init__(self):
-        self._client_id = None
-        self._aws_host = None
-        self._aws_port = None
-        self._root_CA_path = None
-        self._private_key_path = None
-        self._cert_path = None
-        self._socket_ip = None
-        self._socket_port = None
         self._myAWSIoTMQTTClient = None
         self._socket_connection = None
 
     @classmethod
-    def start(cls, info_file):
+    def start(cls, mqtt_param_file=None, socket_param_file=None):
         """Start the socket server and connect the MQTT client.
+
         Arguments:
-        info_file (str): Fully qualified name of file containing all
-                        the information required to start the services.
+        mqtt_param_file (str): Fully qualified name of file containing MQTT params
+        socket_param_file (str): Fully qualified name of file containing socket params
         """
         mqtt_client = cls()
-        connection_parameters = mqtt_client._get_mqtt_parameters_from_file(info_file)
-        mqtt_client._connect_MQTT(**connection_parameters)
-        set_trace()
+        socket_params = mqtt_client._get_parameters_from_file(socket_param_file)
+        mqtt_client._socket_connection = mqtt_client._connect_socket(**socket_params)
+        mqtt_params = mqtt_client._get_parameters_from_file(mqtt_param_file)
+        mqtt_client._myAWSIoTMQTTClient = mqtt_client._connect_MQTT(**mqtt_params)
 
-    def _get_mqtt_parameters_from_file(self, filename):
-        """Get the AWS MQTT connection parameters from a file.
+        #mqtt_client._myAWSIoTMQTTClient.publish("test", json.dumps({"message": "hello world"}), 1)
+
+        while True:
+            message =  mqtt_client._socket_connection.recv(1024)
+            message = message.decode("utf-8")
+            mqtt_client._myAWSIoTMQTTClient.publish("test", message, 1)
+            time.sleep(2)
+
+    def _get_parameters_from_file(self, filename):
+        """Get the parameters from a JSON file
 
         Arguments:
         filename (str): Fully qualified name of the parameter file.
@@ -68,19 +70,25 @@ class MQTTClient(object):
         client.connect()
         return client
 
-    def _connect_socket(self, ip, port):
+    def _connect_socket(self, ip="", port=50007):
         """Create a local socket server.
 
         Arguments:
+        ip (string): IP address of host.
         port (int): Port number for socket server to listen.
 
         Returns a socket connection.
         """
+        print("socket")
         host_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         host_socket.bind((ip, port))
         host_socket.listen(1)
+        print("listen on {}:{}".format(ip, port))
         conn, addr = host_socket.accept()
+        print("connected {}".format(addr))
         return conn
 
 if __name__=="__main__":
-    MQTTClient.start("connection_info.json")
+    print("test")
+    MQTTClient.start(mqtt_param_file="mqtt_connection_info.json",
+                     socket_param_file="socket_connection_info.json")
